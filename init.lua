b9e8a5e542eaaf8a3aed8e5211249ba33742ab0d
Martin Ortbauer <mortbauer@gmail.com>
2021-12-14 21:50:29 +0100

    init

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+require('lsp_config')
+
+return require('packer').startup(function()
+  -- Plugins can have post-install/update hooks
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  use { 'neovim/nvim-lspconfig' }
+
+  use { 'hrsh7th/nvim-cmp' }
+end)
+
