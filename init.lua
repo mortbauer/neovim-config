f29f84060d1dfe10bdaa7df6180ade0b30f6243b
Martin Ortbauer <mortbauer@gmail.com>
2022-01-16 12:07:23 +0100

    make neovim work well

--- init.lua
+++ init.lua
@@ -3,17 +3,109 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
+vim.o.mouse = 'a'
+
+require('monokai').setup { palette = require('monokai').pro }
+
+vim.wo.relativenumber = true
+
+vim.g.autochdir = true
+
+vim.g.mapleader = ','
+
+--Open a file (relative to the current file)
+--See: http://vimcasts.org/episodes/the-edit-command/
+--Synonyms: {e: edit, where: {w: window, s: split, v: vertical split, t: tab}}
+vim.api.nvim_set_keymap("c", "%%", "<C-R>=expand('%:h').'/'<cr>", { noremap = true } )
+vim.api.nvim_set_keymap("n", "<leader>ew", ":e %%", { noremap = false } )
+vim.api.nvim_set_keymap("n", "<leader>es", ":sp %%", { noremap = false } )
+vim.api.nvim_set_keymap("n", "<leader>ev", ":vsp %%", { noremap = false } )
+
+--Replace the word under the cursor 
+vim.api.nvim_set_keymap("n", "<leader>R", ":%s/<C-r><C-w>/", { noremap = true } )
+
+vim.api.nvim_set_keymap("n", "<F7>", ":Vista<cr>", { noremap = true } )
+
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+
+require('plugins')
+
 require('lsp_config')
 
-return require('packer').startup(function()
-  -- Plugins can have post-install/update hooks
-  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+require('nightfox').load('nightfox')
 
-  -- Post-install/update hook with neovim command
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+require('nvim-tree').setup()
 
-  use { 'neovim/nvim-lspconfig' }
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+-- default mappings
+local list = {
+  { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+  { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
+  { key = "<C-v>",                        cb = tree_cb("vsplit") },
+  { key = "<C-x>",                        cb = tree_cb("split") },
+  { key = "<C-t>",                        cb = tree_cb("tabnew") },
+  { key = "<",                            cb = tree_cb("prev_sibling") },
+  { key = ">",                            cb = tree_cb("next_sibling") },
+  { key = "P",                            cb = tree_cb("parent_node") },
+  { key = "<BS>",                         cb = tree_cb("close_node") },
+  { key = "<S-CR>",                       cb = tree_cb("close_node") },
+  { key = "<Tab>",                        cb = tree_cb("preview") },
+  { key = "K",                            cb = tree_cb("first_sibling") },
+  { key = "J",                            cb = tree_cb("last_sibling") },
+  { key = "I",                            cb = tree_cb("toggle_ignored") },
+  { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+  { key = "R",                            cb = tree_cb("refresh") },
+  { key = "a",                            cb = tree_cb("create") },
+  { key = "d",                            cb = tree_cb("remove") },
+  { key = "D",                            cb = tree_cb("trash") },
+  { key = "r",                            cb = tree_cb("rename") },
+  { key = "<C-r>",                        cb = tree_cb("full_rename") },
+  { key = "x",                            cb = tree_cb("cut") },
+  { key = "c",                            cb = tree_cb("copy") },
+  { key = "p",                            cb = tree_cb("paste") },
+  { key = "y",                            cb = tree_cb("copy_name") },
+  { key = "Y",                            cb = tree_cb("copy_path") },
+  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+  { key = "[c",                           cb = tree_cb("prev_git_item") },
+  { key = "]c",                           cb = tree_cb("next_git_item") },
+  { key = "-",                            cb = tree_cb("dir_up") },
+  { key = "s",                            cb = tree_cb("system_open") },
+  { key = "q",                            cb = tree_cb("close") },
+  { key = "g?",                           cb = tree_cb("toggle_help") },
+}
 
-  use { 'hrsh7th/nvim-cmp' }
-end)
+require('nvim-treesitter.configs').setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "maintained",
 
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing
+  ignore_install = { },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    disable = { },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+}
---
+++ lua/plugins.lua
@@ -0,0 +1,33 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Plugins can have post-install/update hooks
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  --lsp
+  use { 'neovim/nvim-lspconfig' }
+
+  --completition
+  use { 'hrsh7th/nvim-cmp' }
+
+  --color scheme
+  use { 'EdenEast/nightfox.nvim' }
+
+  --file explorer
+  use { 'kyazdani42/nvim-tree.lua' }
+
+  --sane python folding
+  use { 'eddiebergman/nvim-treesitter-pyfold' }
+
+  --tagbar via lsp
+  use { 'liuchengxu/vista.vim' }
+
+  --molokai colorscheme
+  use { 'tanvirtin/monokai.nvim' }
+end)
